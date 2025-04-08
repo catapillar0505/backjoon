762be67152e1c8a6bd93a7ab8c6e56dd5c6b4def
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-04-08 20:23:14 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,11 @@
+# 첫째 줄에 N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
+n,m = map(int,input().split())
+# 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다.
+basket = [i+1 for i in range(n)]
+#둘째 줄부터 M개의 줄에는 바구니의 순서를 역순으로 만드는 방법이 주어진다. 방법은 i j로 나타내고, 왼쪽으로부터 i번째 바구니부터 j번째 바구니의 순서를 역순으로 만든다는 뜻이다. (1 ≤ i ≤ j ≤ N)
+for _ in range(m):
+  i,j = map(int,input().split())
+  basket[i-1:j] = reversed(basket[i-1:j])
+
+  
+print(*basket)
