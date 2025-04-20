13ad99dd2569e6c279dc4d210693cc9cefe5435c
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-04-20 20:50:47 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -6,9 +6,13 @@ max_len = max(len(row) for row in a)
 
 # 세로줄만 출력하기
 # j가 행, i가 열
-for i in range(max_len):
-  for j in range(5):
+for i in range(max_len): # 행은 가장 긴 문자열의 길이
+  for j in range(5): # 열은 무조건 5개
     if i<len(a[j]): #인덱스 관리 ->  열이 3개인지, 4개인지
+        # i <= len(a[j])이면 안되는 이유
+        # i는 인덱스 len은 길이잖아
+        # 길이가 4라고 하면
+        # 0 < 4 , 1 < 4, 2 < 4, 3 < 4 이미 인덱스 끝남.. 같아지면 안돼
       print(a[j][i], end="") # 인덱스 조건을 만족할 때만 출력하기
 
 
